1bc07af6cafa3a1148b57d4501fa1b015453bbdb
Marc HEBERT <mheber33@trusty>
2015-03-17 14:37:26 -0400

    First commit. Basic setup.

---
+++ sfs_api.h
@@ -0,0 +1,11 @@
+#define MAXFILENAME 13
+
+int mksfs(int fresh);
+int sfs_fopen(char *name);
+int sfs_fclose(int fileID);
+int sfs_fwrite(int fileID, const char *buf, int length);
+int sfs_fread(int fileID, char *buf, int length);
+int sfs_fseek(int fileID, int offset);
+int sfs_remove(char *file);
+int sfs_get_next_filename(char* filename);
+int sfs_GetFileSize(const char* path);
