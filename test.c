561b89d8de15dd49e91f2cd0e1abbd6639d4f6ac
Marc HEBERT <mheber33@trusty>
2015-03-23 00:01:06 -0400

    done for the night

--- test.c
+++ test.c
@@ -4,14 +4,6 @@
 
 int main (int argc, char* args[])
 {
-	char x = x;
-	short int y = 12;
-	long z = 65;
-	printf("byte : %zu\n".sizeof(x));
-	printf("half : %zu\n".sizeof(y));
-	printf("word : %zu\n",sizeof(z));
-
-	return 0;
-
+	printf("Size of int: %zu", sizeof(unsigned int));
 
 }
